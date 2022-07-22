c35440cafcce4a123f8b65f20a5247059c99aa7b
temitope <adesiyantope2014@gmail.com>
2022-07-22 06:37:56 +0100

    fixed styled component error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
-import Home from './Home';
+import React, { useState } from "react";
+import Home from "./Home";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./themes/global";
+import { lightTheme, darkTheme } from "./themes/theme";
+
+import "./App.css";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
   return (
-    <Home />
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <button onClick={themeToggler}>Switch Theme</button>
+      <Home />
+    </ThemeProvider>
   );
 }
 
